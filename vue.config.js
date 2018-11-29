74a641921c3e3e07b9d43edb86f016d53f3c1e92
xuyanyfb <821224>
2018-11-29 11:54:31 +0800

    111

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,8 @@
+const addData = require('./data')
+const seller = addData.seller
+const goods = addData.goods
+const ratings = addData.ratings
+
 module.exports = {
   css: {
     loaderOptions: {
@@ -13,6 +18,28 @@ module.exports = {
     'cube-ui': {
       postCompile: true,
       theme: true
+    }
+  },
+  devServer: {
+    before(app) {
+      app.get('/api/seller', function (req, res) {
+        res.json({
+          errno: 0,
+          data: seller
+        })
+      })
+      app.get('/api/goods', function (req, res) {
+        res.json({
+          errno: 0,
+          data: goods
+        })
+      })
+      app.get('/api/ratings', function (req, res) {
+        res.json({
+          errno: 0,
+          data: ratings
+        })
+      })
     }
   }
 }
