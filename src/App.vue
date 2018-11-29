ad2fcd0491bdb39fc3272f65e397617b078895d8
xuyanyfb <821224>
2018-11-29 18:26:13 +0800

    11

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,17 @@
 <template>
-  <div id="app">www
+  <div id="app">
+    <vheader></vheader>
   </div>
 </template>
 
-<script>
+<script type="text/ecmascript-6">
+  import Vheader from 'components/v-header/v-header'
 
-
-export default {
-  name: 'app',
-  components: {
-
+  export default {
+    name: 'app',
+    components: {
+      Vheader
+    }
   }
-}
 </script>
-<style lang="stylus">
-#app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
-</style>
+
---
+++ src/api/index.js
@@ -0,0 +1,5 @@
+import {get} from './helper'
+
+export {
+
+}
